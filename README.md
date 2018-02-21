afa13ef6fbbe4a109c67406ffde024b8bb80ea04
Igor Gilitschenski <igilitschenski@mit.edu>
2018-02-21 11:06:53 -0500

    Cleanup and Documentation.

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # carla_binary_catkin
+
+This repository contains a catkin package that automatically downloads and installs the (binaries of the) Carla simulator for autonomous driving research.
+
+## Installation
+
+If you have no catkin workspace, this package can be installed with the following commands.
+
+```shell
+source /opt/ros/kinetic/setup.bash
+mkdir -p /path/to/carla_ws/src
+cd /path/to/carla_ws
+catkin init
+cd /path/to/carla_ws/src
+git clone git@github.com:mit-drl/carla_binary_catkin.git
+catkin build carla_binary_catkin
+```
+
+## Running
+
+First make sure, that setup.bash from ros and your workspace are sourced.
+
+```shell
+source /opt/ros/kinetic/setup.bash
+source /path/to/carla_ws/devel/setup.bash
+```
+
+Then, you can use rosrun to run the Simulator
+
+```shell
+rosrun carla_binary_catkin CarlaUE4
+```
--- download_from_gdrive.py
+++ download_from_gdrive.py
@@ -1,13 +1,20 @@
 """A Scrapy-based gdrive downloader ignoring the large file warning"""
-
+from __future__ import print_function
+import logging
+import sys
 import scrapy
 from scrapy.crawler import CrawlerProcess
 from scrapy.http import Request
 
+
 class GoogleDriveSpider(scrapy.Spider):
     """Small spider for downloading large files from gdrive"""
     name = 'Google Drive Large File Downloader'
-    start_urls = ['https://drive.google.com/uc?id=1IOh3HWQAU2kcdpyPxWfNaNAnwCaUKv2P']
+
+
+    def __init__(self, url, file_name):
+        self.file_name = file_name
+        self.start_urls = [url]
 
     def parse(self, response):
         """Parses Google's warning page."""
@@ -19,14 +26,15 @@ class GoogleDriveSpider(scrapy.Spider):
 
     def save_file(self, response):
         """Saves downloaded file."""
-        name = 'CARLA_0.7.1.tar.gz'
-        with open(name, 'wb') as large_file:
+        with open(self.file_name, 'wb') as large_file:
             large_file.write(response.body)
 
 
 if __name__ == "__main__":
+    # Reduce Scrapy logger verbosity.
+    logging.disable(logging.WARNING)
     spiderproc = CrawlerProcess({
         'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
     })
-    spiderproc.crawl(GoogleDriveSpider)
+    spiderproc.crawl(GoogleDriveSpider, url=sys.argv[1], file_name=sys.argv[2])
     spiderproc.start()
